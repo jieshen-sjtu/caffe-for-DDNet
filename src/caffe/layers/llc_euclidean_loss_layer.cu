6b69dbdc22f4a41e542763779d28d287437bb546
Jie Shen <jieshen.sjtu@gmail.com>
2014-03-07 21:20:10 +0800

    some flaw in fine tune

--- src/caffe/layers/llc_euclidean_loss_layer.cu
+++ src/caffe/layers/llc_euclidean_loss_layer.cu
@@ -32,7 +32,16 @@ namespace caffe
         bottom[0]->height(), bottom[0]->width());
 
     // check if we want fine-tune
+    /*
+     if(this->start_fine_tune_)
+     CHECK_EQ(top->size(), 1) << "LLC Loss Layer takes one output for fine tune";
+     else
+     CHECK_EQ(top->size(), 0);
+     */
+
     CHECK_EQ(top->size(), 1) << "LLC Loss Layer takes one output for fine tune";
+    (*top)[0]->Reshape(bottom[0]->num(), bottom[0]->channels(),
+        bottom[0]->height(), bottom[0]->width());
   }
 
   template<typename Dtype>
@@ -87,32 +96,32 @@ namespace caffe
   }
 
   /*
-  template<typename Dtype>
-  Dtype LLCEuclideanLossLayer<Dtype>::Backward_gpu(
-      const vector<Blob<Dtype>*>& top, const bool propagate_down,
-      vector<Blob<Dtype>*>* bottom)
-  {
-    if (this->start_fine_tune())
-    {
-      const Dtype* const top_diff = top[0]->gpu_diff();
-      Dtype* const bottom_diff = (*bottom)[0]->mutable_gpu_diff();
-      caffe_gpu_copy(top[0]->count(), top_diff, bottom_diff);
+   template<typename Dtype>
+   Dtype LLCEuclideanLossLayer<Dtype>::Backward_gpu(
+   const vector<Blob<Dtype>*>& top, const bool propagate_down,
+   vector<Blob<Dtype>*>* bottom)
+   {
+   if (this->start_fine_tune())
+   {
+   const Dtype* const top_diff = top[0]->gpu_diff();
+   Dtype* const bottom_diff = (*bottom)[0]->mutable_gpu_diff();
+   caffe_gpu_copy(top[0]->count(), top_diff, bottom_diff);
 
-      return Dtype(0.);
-    }
+   return Dtype(0.);
+   }
 
-    int count = (*bottom)[0]->count();
-    int num = (*bottom)[0]->num();
-    caffe_sub(count, (*bottom)[0]->cpu_data(), (*bottom)[1]->cpu_data(),
-              difference_.mutable_cpu_data());
-    Dtype loss = caffe_cpu_dot(count, difference_.cpu_data(),
-                               difference_.cpu_data()) / num / Dtype(2);
-    // Compute the gradient
-    caffe_axpby(count, Dtype(1) / num, difference_.cpu_data(), Dtype(0),
-                (*bottom)[0]->mutable_cpu_diff());
-    return loss;
-  }
-  */
+   int count = (*bottom)[0]->count();
+   int num = (*bottom)[0]->num();
+   caffe_sub(count, (*bottom)[0]->cpu_data(), (*bottom)[1]->cpu_data(),
+   difference_.mutable_cpu_data());
+   Dtype loss = caffe_cpu_dot(count, difference_.cpu_data(),
+   difference_.cpu_data()) / num / Dtype(2);
+   // Compute the gradient
+   caffe_axpby(count, Dtype(1) / num, difference_.cpu_data(), Dtype(0),
+   (*bottom)[0]->mutable_cpu_diff());
+   return loss;
+   }
+   */
 
   INSTANTIATE_CLASS(LLCEuclideanLossLayer);
 }
